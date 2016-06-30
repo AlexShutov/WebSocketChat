11f9804b10556f64936cb4d6a1faa7172aa06cb2
User <lodoss@lodoss.com>
2016-06-30 11:40:55 +0300

    socket.io library different version

--- app/src/main/java/alex_shutov/com/websocketchat/Web/SocketManager.java
+++ app/src/main/java/alex_shutov/com/websocketchat/Web/SocketManager.java
@@ -5,6 +5,21 @@ import android.util.EventLog;
 import android.util.Log;
 import android.util.Printer;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.CookieManager;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
+import java.security.NoSuchAlgorithmException;
+
+import javax.net.ssl.SSLContext;
+
+import io.socket.IOAcknowledge;
+import io.socket.IOCallback;
+import io.socket.SocketIO;
+import io.socket.SocketIOException;
+import io.socket.client.IO;
 import io.socket.client.Socket;
 import io.socket.emitter.Emitter;
 
@@ -15,8 +30,9 @@ public class SocketManager {
     private static final String LOG_TAG = SocketManager.class.getSimpleName();
     private Context context;
     private String baseUrl;
-    Socket socketСlient;
     String token;
+
+    SocketIO socket;
 
     public SocketManager(Context context){
         this.context = context;
@@ -24,48 +40,52 @@ public class SocketManager {
     }
 
     public void connect(){
-        token = LoginManager.getToken(context);
-
-        socketСlient = createSocket(baseUrl, token);
-
-        socketСlient.on(Socket.EVENT_CONNECT, onConnect);
-        socketСlient.on(Socket.EVENT_DISCONNECT, onDisconnect);
-        socketСlient.on("connection", new Emitter.Listener() {
+        try {
+            SocketIO.setDefaultSSLSocketFactory(SSLContext.getDefault());
+        }catch (NoSuchAlgorithmException e){
+            e.printStackTrace();
+        }
+        socket.connect(new IOCallback() {
             @Override
-            public void call(Object... args) {
-                Log.i(LOG_TAG, "connected: connection ");
+            public void onDisconnect() {
+                Log.i(LOG_TAG, "onConnect");
             }
-        });
-        socketСlient.on(Socket.EVENT_CONNECT_ERROR, new Emitter.Listener() {
-            @Override
-            public void call(Object... args) {
-                Log.i(LOG_TAG, "connection error: " + args[0].toString());
-            }
-        });
 
-        socketСlient.on("getMyInfo", new Emitter.Listener() {
             @Override
-            public void call(Object... args) {
-                Log.i(LOG_TAG, "'getMyInfo' callback fired");
+            public void onConnect() {
+                Log.i(LOG_TAG, "onDisconnect");
+                socket.emit("getMyInfo", "123");
             }
-        });
 
-        socketСlient.on("authenticate", new Emitter.Listener() {
             @Override
-            public void call(Object... args) {
-                Log.i(LOG_TAG, "authenticate");
+            public void onMessage(String s, IOAcknowledge ioAcknowledge) {
+                Log.i(LOG_TAG, "onMessage: " + s);
             }
-        });
 
-        socketСlient.connect();
+            @Override
+            public void onMessage(JSONObject jsonObject, IOAcknowledge ioAcknowledge) {
+                Log.i(LOG_TAG, "onMessage: " + jsonObject.toString());
+            }
+
+            @Override
+            public void on(String s, IOAcknowledge ioAcknowledge, Object... objects) {
+                Log.i(LOG_TAG, "onMessage: " + s);
+            }
+
+            @Override
+            public void onError(SocketIOException e) {
+                Log.i(LOG_TAG, "onError");
+            }
+
+        });
     }
 
     /**
      * append token as additional parameter, force creationi of new socket on connect
      * @return new socket instance
      */
-    Socket createSocket(String baseUrl, String token){
-        /**
+    SocketIO createSocket(String baseUrl, String token){
+
         JSONObject params = new JSONObject();
         try {
             params.put("token", token);
@@ -74,48 +94,30 @@ public class SocketManager {
         IO.Options options = new IO.Options();
         options.forceNew = true;
         options.query = params.toString();
-        Socket client = null;
+
+        //String cookie = android.webkit.CookieManager.getInstance().getCookie(baseUrl);
+        SocketIO socket = null;
         try {
-            client = IO.socket(baseUrl, options);
-        } catch (URISyntaxException e){
-            throw new RuntimeException(e);
+            socket = new SocketIO(baseUrl);
+        } catch (MalformedURLException e){
+            e.printStackTrace();
+            Log.i(LOG_TAG, "error parsing base url");
         }
-        return client;
-         */
+        if (null == socket) return socket;
+
+        socket.addHeader("token", token);
 
 
-
-
-        return null;
+        return socket;
     }
-    
-
-    private Emitter.Listener onConnect = new Emitter.Listener() {
-        @Override
-        public void call(Object... args) {
-            int N = 20;
-            Log.i(LOG_TAG, "onConnect ");
-            socketСlient.emit("getMyInfo", onMyInfo);
-        }
-    };
-
-    private Emitter.Listener onMyInfo = new Emitter.Listener() {
-        @Override
-        public void call(Object... args) {
-            Log.i(LOG_TAG, "onMyInfo");
-        }
-    };
-
-
-    private Emitter.Listener onDisconnect = new Emitter.Listener() {
-        @Override
-        public void call(Object... args) {
-            Log.i(LOG_TAG, "onDisconnect " + args[0].toString() );
-        }
-    };
 
     public void testConnection(){
-        String token = LoginManager.getToken(context);
+        token = LoginManager.getToken(context);
+        socket = createSocket(baseUrl, token);
+        if (null == socket){
+            Log.i(LOG_TAG, "Socket instance is null");
+            return;
+        }
         connect();
     }
 
